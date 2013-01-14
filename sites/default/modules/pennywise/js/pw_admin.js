357903695c39287c440e877c6aa00d06c9f637ef
Kendrick <wallncrown@mac.com>
2013-01-14 15:43:52 -0800

    Hot Fix - Patch for CSS

--- sites/default/modules/pennywise/js/pw_admin.js
+++ sites/default/modules/pennywise/js/pw_admin.js
@@ -1,11 +1,10 @@
-//(function($)){
-  jQuery(document).ready(function() {
-    // Hide all content that needs to hide.
-    jQuery('.ad-details-toggle-box-content').hide();
+// Toggle For Hiding/Unhiding Ad Submission 'More' Data
+jQuery(document).ready(function() {
+  // Hide all content that needs to hide.
+  jQuery('.ad-details-toggle-box-content').hide();
 
-    // When switch is activated use it's CLASS and then find it's ID pair and toggle it.
-    jQuery('[id^=ad-details-toggle-box-switch-]').click(function() {
-      jQuery('#ad-details-'+(jQuery(this).attr('class'))).toggle('slow');
-    });
-  })
-//}(jQuery));
+  // When switch is activated use it's CLASS and then find it's ID pair and toggle it.
+  jQuery('[id^=ad-details-toggle-box-switch-]').click(function() {
+    jQuery('#ad-details-'+(jQuery(this).attr('class'))).toggle('slow');
+  });
+})
